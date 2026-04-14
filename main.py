d39ccc47b200cffb151d46b6314158486ab857b0
Paola <paolaleonardiruvinski@gmail.com>
2026-04-14 17:04:32 -0300

    Primeiros passos para iniciar main.py
    Implementei uma versão simples do menú

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-# --------------- Menú ----------------- ---------
-
+# --------------- Menú ---------------------------
+print ("JOKENPÔ")
+print ("1: Humano x Humano ")
+print ("2: Computador x Humano ")
+print ("3: Computador x Computador ")
+print ("0: SAIR")
+modo = int (input("Digite o número do modo que gostaria de jogar:\n> "))
+#-
 # --------------- Helena: Humano x Humano --------
 
 # --------------- Juliana: Computador x Humano ---
 
 # --------------- Paola: Computador x Computador -
+if modo == 3:
+    print ("MODO: Computador VS Computador selecionado")

4c79184ed48c0348f794723cf252dfb16d402097
Paola <paolaleonardiruvinski@gmail.com>
2026-04-14 16:40:47 -0300

    Update main.py
    TESTE

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-Criação da main principal 
+# --------------- Menú ----------------- ---------
+
+# --------------- Helena: Humano x Humano --------
+
+# --------------- Juliana: Computador x Humano ---
+
+# --------------- Paola: Computador x Computador -

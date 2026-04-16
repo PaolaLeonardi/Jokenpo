68450bf90a83d442521c0c5fe2f1f1fed6214a5b
Helena Gomes <helenapbgomes@gmail.com>
2026-04-16 11:23:01 -0300

    1.5 fixed some parts

--- main.py
+++ main.py
@@ -49,52 +49,52 @@ while modo == 1:
 # --------------- Juliana: Computador x Humano ---
 
 # --------------- Paola: Computador x Computador -
-# import random
-#
-# pontos1 = 0
-# pontos2 = 0
-#
-# if modo == 3:
-#     print("MODO: Computador VS Computador selecionado")
-#
-#     while True:
-#         jogada1 = random.randint(1, 3)
-#         jogada2 = random.randint(1, 3)
-#
-#         if jogada1 == 1:
-#             print("Jogador 1: Pedra")
-#         elif jogada1 == 2:
-#             print("Jogador 1: Papel")
-#         else:
-#             print("Jogador 1: Tesoura")
-#
-#         if jogada2 == 1:
-#             print("Jogador 2: Pedra")
-#         elif jogada2 == 2:
-#             print("Jogador 2: Papel")
-#         else:
-#             print("Jogador 2: Tesoura")
-#
-#         if jogada1 == jogada2:
-#             print("Empate!")
-#
-#         elif (jogada1 == 1 and jogada2 == 3) or \
-#              (jogada1 == 3 and jogada2 == 2) or \
-#              (jogada1 == 2 and jogada2 == 1):
-#             print("Jogador 1 venceu!")
-#             pontos1 += 1
-#         else:
-#             print("Jogador 2 venceu!")
-#             pontos2 += 1
-#         print(f"Placar final: {pontos1} x {pontos2}")
-#         denovo = str (input("Deseja jogar novamente? N/Y\n>").lower())
-#
-#         if denovo != "y":
-#             break
-#
-#     print("\n=== RESULTADO FINAL ===")
-#     print(f"Jogador 1: {pontos1}")
-#     print(f"Jogador 2: {pontos2}")
-#
-#     print ("\nObrigada por jogar!\n")
-#     print ("Alunas responsáveis:\nPaola R. Leonardi\nHelena Gomes\nJuliana Munhoz")
+import random
+
+pontos1 = 0
+pontos2 = 0
+
+if modo == 3:
+    print("MODO: Computador VS Computador selecionado")
+
+    while True:
+        jogada1 = random.randint(1, 3)
+        jogada2 = random.randint(1, 3)
+
+        if jogada1 == 1:
+            print("Jogador 1: Pedra")
+        elif jogada1 == 2:
+            print("Jogador 1: Papel")
+        else:
+            print("Jogador 1: Tesoura")
+
+        if jogada2 == 1:
+            print("Jogador 2: Pedra")
+        elif jogada2 == 2:
+            print("Jogador 2: Papel")
+        else:
+            print("Jogador 2: Tesoura")
+
+        if jogada1 == jogada2:
+            print("Empate!")
+
+        elif (jogada1 == 1 and jogada2 == 3) or \
+             (jogada1 == 3 and jogada2 == 2) or \
+             (jogada1 == 2 and jogada2 == 1):
+            print("Jogador 1 venceu!")
+            pontos1 += 1
+        else:
+            print("Jogador 2 venceu!")
+            pontos2 += 1
+        print(f"Placar final: {pontos1} x {pontos2}")
+        denovo = str (input("Deseja jogar novamente? N/Y\n>").lower())
+
+        if denovo != "y":
+            break
+
+    print("\n=== RESULTADO FINAL ===")
+    print(f"Jogador 1: {pontos1}")
+    print(f"Jogador 2: {pontos2}")
+
+    print ("\nObrigada por jogar!\n")
+    print ("Alunas responsáveis:\nPaola R. Leonardi\nHelena Gomes\nJuliana Munhoz")
